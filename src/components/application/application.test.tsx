62996cb2065ee19335bbc6ffcadbe400bd7b28d5
Jonty Sachdeva <er.jontysachdeva@gmail.com>
2024-07-10 12:55:42 +0530

    getBy query tests

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -1,46 +1,80 @@
-import { render, screen } from "@testing-library/react"
-import { Application } from "./application"
-import React from 'react';
+import { render, screen } from "@testing-library/react";
+import { Application } from "./application";
 
-describe('Application', () => {
-    it('renders correctly', () => {
-        render(<Application />);
+describe("Application", () => {
+  test("renders", () => {
+    render(<Application />);
 
-        const pageHeading = screen.getByRole('heading', { level: 1 });
-        expect(pageHeading).toBeInTheDocument();
+    /** getByRole */
 
-        const sectionHeading = screen.getByRole('heading', { level: 2 });
-        expect(sectionHeading).toBeInTheDocument();
+    const pageHeading = screen.getByRole("heading", {
+      level: 1,
+    });
+    expect(pageHeading).toBeInTheDocument();
 
-        const mandatoryElement = screen.getByText('All fields are mandatory');
-        expect(mandatoryElement).toBeInTheDocument();
+    const sectionHeading = screen.getByRole("heading", {
+      level: 2,
+    });
+    expect(sectionHeading).toBeInTheDocument();
 
-        const closeElement = screen.getByTitle('close');
-        expect(closeElement).toBeInTheDocument();
+    const nameElement = screen.getByRole("textbox", {
+      name: "Name",
+    });
+    expect(nameElement).toBeInTheDocument();
 
-        const customElement = screen.getByTestId('custom-element');
-        expect(customElement).toBeInTheDocument();
+    const bioElement = screen.getByRole("textbox", {
+      name: "Bio",
+    });
+    expect(bioElement).toBeInTheDocument();
 
-        const nameElement =  screen.getByRole('textbox', {
-            name: 'Name'
-        });
-        expect(nameElement).toBeInTheDocument();
+    const jobLocationElement = screen.getByRole("combobox");
+    expect(jobLocationElement).toBeInTheDocument();
 
-        const nameElement2 = screen.getByDisplayValue('Jonty Sachdeva');
-        expect(nameElement2).toBeInTheDocument();
+    const termsElement = screen.getByRole("checkbox");
+    expect(termsElement).toBeInTheDocument();
 
-        const bioElement = screen.getByRole('textbox', {
-            name: 'Bio'
-        });
-        expect(bioElement).toBeInTheDocument()
+    const submitElement = screen.getByRole("button");
+    expect(submitElement).toBeInTheDocument();
+    expect(submitElement).toBeDisabled();
 
-        const jobLocationElement = screen.getByRole('combobox');
-        expect(jobLocationElement).toBeInTheDocument();
+    /** getByLabelText */
 
-        const termsElement = screen.getByRole('checkbox');
-        expect(termsElement).toBeInTheDocument();
+    const nameElement2 = screen.getByLabelText("Name", { selector: "input" });
+    expect(nameElement2).toBeInTheDocument();
 
-        const submitButtonElement = screen.getByRole('button');
-        expect(submitButtonElement).toBeInTheDocument();
-    })
-})
+    const termsElement2 = screen.getByLabelText(
+      "I agree to the terms and conditions"
+    );
+    expect(termsElement2).toBeInTheDocument();
+
+    /** getByPlaceholderText */
+
+    const nameElement3 = screen.getByPlaceholderText("Fullname");
+    expect(nameElement3).toBeInTheDocument();
+
+    /** getByText */
+
+    const paragraphElement = screen.getByText("All fields are mandatory");
+    expect(paragraphElement).toBeInTheDocument();
+
+    /** getByDisplayValue */
+
+    const nameElement4 = screen.getByDisplayValue("Jonty");
+    expect(nameElement4).toBeInTheDocument();
+
+    /** getByAltText */
+
+    const imageElement = screen.getByAltText("a person with a laptop");
+    expect(imageElement).toBeInTheDocument();
+
+    /** getByTitle */
+
+    const closeElement = screen.getByTitle("close");
+    expect(closeElement).toBeInTheDocument();
+
+    /** getByTestId */
+
+    const customElement = screen.getByTestId("custom-element");
+    expect(customElement).toBeInTheDocument();
+  });
+});
