c522fdace4bddc04ceabaad3e47b07f14b54f1b1
Jonty Sachdeva <er.jontysachdeva@gmail.com>
2024-07-11 16:03:16 +0530

    custom hook tests

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { Application } from "./application";
+import React from 'react';
 
 describe("Application", () => {
   test("renders", () => {
---
+++ src/hooks/useCounter/useCounter.test.tsx
@@ -0,0 +1,32 @@
+import { act, renderHook } from "@testing-library/react";
+import { useCounter } from "./useCounter";
+
+describe("useCounter", () => {
+  it("renders the initial count", () => {
+    const { result } = renderHook(useCounter);
+    expect(result.current.count).toBe(0);
+  });
+
+  it("should accept and render the same initial count", () => {
+    const { result } = renderHook(useCounter, {
+      initialProps: {
+        initialCount: 10,
+      },
+    });
+
+    expect(result.current.count).toBe(10);
+  });
+
+  //   act() is a function that makes sure updations are processed before assertions are made
+  it("should increment the count", () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.increment());
+    expect(result.current.count).toBe(1);
+  });
+
+  it("should decrement the count", () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.decrement());
+    expect(result.current.count).toBe(-1);
+  });
+});
